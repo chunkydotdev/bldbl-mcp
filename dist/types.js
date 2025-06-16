2fcb7ceace08d4049ca4e07bbc7f3901d3870208
Magnus Junghard Huttu <magnushuttu@gmail.com>
2025-06-16 21:20:31 +0200

    v1

--- dist/types.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// Buildable MCP Client Types
-// Comprehensive type definitions for AI assistant integration
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=types.js.map
